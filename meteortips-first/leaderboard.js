289b5fc4eea285d86a83e6802a19858571cab4b1
DiMesq <d.mesquita285@gmail.com>
2016-05-17 12:24:20 +0100

    Add complete first meteor app tutorial

--- meteortips-first/leaderboard.js
+++ meteortips-first/leaderboard.js
@@ -1,9 +1,56 @@
 PlayersList = new Mongo.Collection('players');
 
+Meteor.methods({
+  'createPlayer': function(playerName) {
+    check(playerName, String);
+    var currentPlayerId = this.userId;
+    if (currentPlayerId) {
+      PlayersList.insert({
+        owner: this.userId,
+        name: playerName,
+        score: 0
+      })
+    }
+  },
+
+  'removePlayer': function(playerId) {
+    check(playerId, String);
+    var currentPlayerId = this.userId;
+
+    if (currentPlayerId) {
+      PlayersList.remove({
+        _id: playerId,
+        owner: currentPlayerId
+      })
+    }
+  },
+
+  'modifyScore': function(playerId, score) {
+    check(score, Match.Integer);
+    check(playerId, String);
+    var currentPlayerId = this.userId;
+
+    if (currentPlayerId && (score == -5 || score == 5)) {
+      PlayersList.update(
+        {
+          _id: playerId,
+          owner: currentPlayerId
+        },
+        {
+          $inc: {score: score}
+        }
+      );
+    }
+  }
+});
+
 if (Meteor.isClient) {
+
+  Meteor.subscribe('thePlayers');
+
   Template.leaderboard.helpers({
     'player': function() {
-      return PlayersList.find({}, {sort: {score: -1, name: 1}});
+      return PlayersList.find({owner: Meteor.userId()}, {sort: {score: -1, name: 1}});
     },
     'selectedPlayer': function(){
       var player = Session.get('selectedPlayer');
@@ -12,15 +59,12 @@ if (Meteor.isClient) {
   });
 
   Template.leaderboard.events({
-    'click .increment': function(e) {
+    'click .modify-score': function(e) {
       var selectedPlayer = Session.get('selectedPlayer');
-      PlayersList.update({_id: selectedPlayer}, {$inc: {score: 5}});
-    },
-    'click .decrement': function(e) {
-      var player = Session.get('selectedPlayer');
-      PlayersList.update({_id: player}, {$inc:{score:-5}});
+      var score = Number($(e.target).data('score'));
+      Meteor.call('modifyScore', selectedPlayer, score)
     }
-  })
+  });
 
   Template.playerDisplay.helpers({
     'selectedClass': function() {
@@ -28,16 +72,36 @@ if (Meteor.isClient) {
       var selectedPlayer = Session.get('selectedPlayer');
       return player == selectedPlayer ? 'selected' : null;
     }
-  })
+  });
 
   Template.playerDisplay.events({
     'click .player': function(e) {
       Session.set('selectedPlayer', this._id);
     },
+    'click .delete-player': function(e){
+      
+      Meteor.call('removePlayer', this._id);
+      e.stopPropagation();
+    }
+  });
+
+  Template.addPlayer.events({
+    'submit .add-player': function(e){
+      e.preventDefault();
+
+      var name = $('.add-player input[name=name]').val();
+      Meteor.call('createPlayer', name);
+      $('.add-player input[name=name]').val('');
+    }
   })
+
 }
 
 if (Meteor.isServer) {
 
+  Meteor.publish('thePlayers', function() {
+    var currentPlayerId = this.userId;
+    return PlayersList.find({owner: currentPlayerId});
+  });
 }
 
